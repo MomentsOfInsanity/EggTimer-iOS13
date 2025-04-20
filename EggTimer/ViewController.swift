18f93c39bc981770856522d631c31c8467bf7f66
Raj Bedi <71458567+MomentsOfInsanity@users.noreply.github.com>
2025-04-20 01:42:29 -0400

    finished egg timer.
    added progress bar and changing text. did not add sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,24 +13,50 @@ class ViewController: UIViewController {
 //    let softTime = 5
 //    let mediumTime = 7
 //    let HardTime = 12
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
+//    var secondsRemaining = 60
+    
+    var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
         
-//        if(hardness == "Soft"){
-//            print(softTime)
-//        }
-//        else if(hardness == "Medium"){
-//            print(mediumTime)
-//        }
-//        else{
-//            print(HardTime)
-//        }
+//        progressBar.progress = 1.0
         
-        print(eggTimes[hardness!] ?? 0
-        )
+        progressBar.progress = 0.0
+        secondsPassed = 0
+       
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
+        
+        @objc func updateTimer() {
+            if secondsPassed < totalTime {
+                
+                secondsPassed += 1
+                progressBar.progress = Float(secondsPassed) / Float(totalTime)
+
+            }
+            else {
+                timer.invalidate()
+                titleLabel.text = "DONE!"
+            }
+            
+        }
+    
     
 
 }
